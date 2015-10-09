1ce83a82c3cfec4092655bdae2b98fc71180111c
git <git@3113479.no-reply.drupal.org>
2015-10-09 13:24:30 +0200

    Issue #2306853 by Arla, sasanikolic: Themable share links

--- src/Entity/Handler/ShareMessageViewBuilder.php
+++ src/Entity/Handler/ShareMessageViewBuilder.php
@@ -9,6 +9,7 @@ namespace Drupal\sharemessage\Entity\Handler;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityViewBuilder;
+use Drupal\Core\Template\Attribute;
 use Drupal\sharemessage\Entity\ShareMessage;
 
 /**
@@ -53,20 +54,29 @@ class ShareMessageViewBuilder extends EntityViewBuilder {
       }
       unset($context['_force_attributes']);
 
+      $attributes = new Attribute(array('class' => array(
+        'addthis_toolbox',
+        'addthis_default_style',
+        !empty($entity->settings['icon_style']) ? $entity->settings['icon_style'] : \Drupal::config('sharemessage.settings')->get('icon_style'),
+      )));
+
+      if ($addThis_attributes) {
+        $attributes['addthis:url'] = $entity->getUrl($context);
+        $attributes['addthis:title'] = $entity->getTokenizedField($entity->title, $context);
+        $attributes['addthis:description'] = $entity->getTokenizedField($entity->message_long, $context);
+      }
+
       // Add addThis buttons.
       if ($view_mode == 'full') {
         $build[$entity->id()]['addthis'] = array(
-          '#type' => 'container',
-          '#attributes' => $addThis_attributes ? $this->buildAdditionalAttributes($entity, $context) : $this->buildAttributes($entity),
-          'services' => array(
-            '#markup' => $this->buildServicesPart($entity, $context),
-          ),
-          'additional_services' => array(
-            '#markup' => $this->buildAdditionalServicesPart($entity),
-          ),
+          '#theme' => 'sharemessage_addthis',
+          '#attributes' => $attributes,
           '#cache' => [
             'tags' => $entity->getCacheTags(),
           ],
+          '#services' => !empty($entity->settings['services']) ? $entity->settings['services'] : \Drupal::config('sharemessage.settings')->get('services'),
+          '#additional_services' =>  isset($entity->settings['additional_services']) ? $entity->settings['additional_services'] : \Drupal::config('sharemessage.settings')->get('additional_services'),
+          '#counter' => isset($entity->settings['counter']) ? $entity->settings['counter'] : \Drupal::config('sharemessage.settings')->get('counter'),
           '#attached' => array(
             'library' => ['sharemessage/addthis'],
             'drupalSettings' => array(
@@ -93,77 +103,6 @@ class ShareMessageViewBuilder extends EntityViewBuilder {
       );
     }
     return $mapped;
-  }
-
-  /**
-   * Function that adds icon style as part of addThis widget.
-   */
-  private function buildAttributes(ShareMessage $entity) {
-    $icon_style = !empty($entity->settings['icon_style']) ? $entity->settings['icon_style'] : \Drupal::config('sharemessage.settings')->get('icon_style');
-    return array(
-      'class' => array('addthis_toolbox', 'addthis_default_style', $icon_style),
-    );
-  }
-
-  /**
-   * Function that adds icon style with addThis:attributes
-   * (url, title, description) as part of addThis widget.
-   */
-  function buildAdditionalAttributes(ShareMessage $entity, $context) {
-    $attributes = $this->buildAttributes($entity);
-    $attributes['addthis:url'] = $entity->getUrl($context);
-    $attributes['addthis:title'] = $entity->getTokenizedField($entity->title, $context);
-    $attributes['addthis:description'] = $entity->getTokenizedField($entity->message_long, $context);
-    return $attributes;
-  }
-
-  /**
-   * Function that adds services as part of addThis widget.
-   */
-  private function buildServicesPart(ShareMessage $entity, $context) {
-    $services = !empty($entity->settings['services']) ? $entity->settings['services'] : \Drupal::config('sharemessage.settings')->get('services');
-
-    // Configured.
-    // @todo render this as render array and attach js using #attached property.
-    $services_HTML = '';
-    if (!empty($services)) {
-      foreach ($services as $key => $service) {
-        if ($key == 'twitter' && $entity->message_short) {
-          $services_HTML .= "<script>
-<!--//--><![CDATA[//><!-- var addthis_share = { templates: { twitter: '" . $entity->getTokenizedField($entity->message_short, $context) . " } } //--><!]]>
-</script>";
-        }
-        $services_HTML .= '<a class="addthis_button_' . $key . '"></a>';
-      }
-    }
-    else {
-      // Default.
-      $services_HTML .= '
-        <a class="addthis_button_preferred_1"></a>
-        <a class="addthis_button_preferred_2"></a>
-        <a class="addthis_button_preferred_3"></a>
-        <a class="addthis_button_preferred_4"></a>
-        <a class="addthis_button_preferred_5"></a>';
-    }
-
-    return $services_HTML;
-  }
-
-  /**
-   * Function that adds additional services as part of addThis widget.
-   */
-  private function buildAdditionalServicesPart($entity) {
-    $additional_services = isset($entity->settings['additional_services']) ? $entity->settings['additional_services'] : \Drupal::config('sharemessage.settings')->get('additional_services');
-
-    $additional = '';
-    if ($additional_services) {
-      $additional .= '<a class="addthis_button_compact"></a>';
-    }
-    $counter = isset($entity->settings['counter']) ? $entity->settings['counter'] : \Drupal::config('sharemessage.settings')->get('counter');
-    if ($counter) {
-      $additional .= '<a class="addthis_counter ' . $counter . '"></a>';
-    }
-    return $additional;
   }
 
 }
